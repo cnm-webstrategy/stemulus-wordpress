0e583e245ba242e3beeffc1c044be68cd7376119
eugenehiggins <genotype@gknow.com>
2016-02-22 10:46:14 -0700

    about to fix error in Awaken theme parent

--- wp-content/themes/themetrust-create-child/header.php
+++ wp-content/themes/themetrust-create-child/header.php
@@ -27,20 +27,20 @@ $header_class .= $header_color_scheme;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <!-- Google Tag Manager -->
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WNQNLQ"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFVJGC"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WNQNLQ');</script>
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KFVJGC');</script>
     <!-- End Google Tag Manager -->
 	
 	<?php create_loader(); ?>
