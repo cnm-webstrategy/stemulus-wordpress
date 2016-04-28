bc4f4e1f48cdaefe03419b6a1c7673f00c5ca069
eugenehiggins <genotype@gknow.com>
2016-04-28 16:13:09 -0600

    removed plugins directory

--- wp-content/plugins/wp-booking-calendar/public/ajax/getCalendar.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../common.php';
-$bookingCalendarObj->setCalendar($_GET["calendar_id"]);
-?>
-<?php echo $bookingCalendarObj->getFirstFilledMonth($bookingCalendarObj->getCalendarId()); ?>
