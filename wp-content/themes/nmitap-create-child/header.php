e15dad3a7c0fec4aecb59a1056c6a52a950806ec
eugenehiggins <genotype@gknow.com>
2016-05-04 15:02:43 -0600

    added new child theme for NMITAP, added google tracking code for it

--- wp-content/themes/nmitap-create-child/header.php
+++ wp-content/themes/nmitap-create-child/header.php
@@ -34,6 +34,17 @@ $header_class .= $header_color_scheme;
 
 <body <?php body_class(); ?>>
 
+	<!-- Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PN2ZP3"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PN2ZP3');</script>
+	<!-- End Google Tag Manager —> 
+
+	
 	<?php create_loader(); ?>
 	<?php create_scroll_to_top(); ?>
 	
