aa36a5a1251ebdf7d32268a3e2ffa4bac39ca61a
eugenehiggins <genotype@gknow.com>
2016-06-09 14:55:05 -0600

    changed custom admin theme to only load in staging

--- wp-content/plugins/custom-admin-theme/custom_admin_theme.php
+++ wp-content/plugins/custom-admin-theme/custom_admin_theme.php
@@ -12,7 +12,10 @@ Author URI: http://example.com
 function my_admin_theme_style() {
     wp_enqueue_style('my-admin-theme', plugins_url('wp-admin.css', __FILE__));
 }
-add_action('admin_enqueue_scripts', 'my_admin_theme_style');
-add_action('login_enqueue_scripts', 'my_admin_theme_style');
+
+if (is_wpe_snapshot()) {
+	add_action('admin_enqueue_scripts', 'my_admin_theme_style');
+	add_action('login_enqueue_scripts', 'my_admin_theme_style');
+}
 
 ?>
