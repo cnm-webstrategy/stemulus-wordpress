7be52ef357cb1e3e4eacfa951cb4a6a569f93dd6
HIGGINS <ehigginsiii@m766181.admin.ad.cnm.edu>
2016-08-24 15:04:49 -0600

    added google tag manager to header.php

--- wp-content/themes/genesis/header.php
+++ wp-content/themes/genesis/header.php
@@ -19,11 +19,14 @@ wp_head(); //* we need this for plugins
 ?>
 </head>
 <?php
+
+
 genesis_markup( array(
 	'html5'   => '<body %s>',
 	'xhtml'   => sprintf( '<body class="%s">', implode( ' ', get_body_class() ) ),
 	'context' => 'body',
 ) );
+
 do_action( 'genesis_before' );
 
 genesis_markup( array(
