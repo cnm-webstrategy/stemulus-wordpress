f562682fbc91acfd7b89196007a3b21fa124d695
HIGGINS <ehigginsiii@m766181.admin.ad.cnm.edu>
2016-09-08 13:53:54 -0600

    first commit after using wpengine's recommended .gitignore

---
+++ wp-content/themes/nmitap-create-child/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+
+    //wp_enqueue_style('google-fonts',"https://fonts.googleapis.com/css?family=Oswald:400,700,300");
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+?>
