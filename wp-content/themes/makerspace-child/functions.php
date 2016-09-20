01aac1cfeedb545aeffc8bc59fd5417fb5d6044e
Gene Higgins <ehigginsiii@cnm.edu>
2016-09-20 16:06:51 -0600

    republished cloud fonts

--- wp-content/themes/makerspace-child/functions.php
+++ wp-content/themes/makerspace-child/functions.php
@@ -8,6 +8,14 @@ function theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
+
+	// is this wpengine production?
+	if ( is_wpe() ) {  
+	    wp_enqueue_style('typography-font', 'https://cnmingenuity.org/fonts/501134/fonts.css');
+	} else  {
+		//development url
+		 wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/785104/css/fonts.css');
+	}
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
