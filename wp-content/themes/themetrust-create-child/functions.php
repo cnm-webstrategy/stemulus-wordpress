5bca262ed44e028b518567906a6b592109489ff8
Gene Higgins <ehigginsiii@cnm.edu>
2016-09-20 16:12:24 -0600

    updated sites with new style url
    Former-commit-id: cfb9d7ffc445ce8910a32c47c8fc2b362badf34c

--- wp-content/themes/themetrust-create-child/functions.php
+++ wp-content/themes/themetrust-create-child/functions.php
@@ -9,6 +9,14 @@ function theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
+
+    	// is this wpengine production?
+	if ( is_wpe() ) {  
+	    wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/641408/css/fonts.css');
+	} else  {
+		//development url
+		 wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/785104/css/fonts.css');
+	}
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
