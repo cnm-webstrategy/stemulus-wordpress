e9eb898f0af5698f71ea39decbd2aae5ea001601
Gene Higgins <ehigginsiii@cnm.edu>
2016-12-12 16:42:52 -0700

    fixed recurring missing fonts, http/https errors

--- wp-content/themes/awaken-child-stemulus/functions.php
+++ wp-content/themes/awaken-child-stemulus/functions.php
@@ -42,4 +42,12 @@ if ( ! function_exists( 'cs_get_font_family' ) ) {
 	}
 }
 
+// is this wpengine production?
+if ( function_exists('is_wpe') && is_wpe() ) {
+    wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/641408/css/fonts.css');
+} else  {
+    //development url
+    wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/785104/css/fonts.css');
+}
+
 ?>
