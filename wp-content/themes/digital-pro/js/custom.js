bbbe3c527e154c4f814d788759eb49a77aa4ba8a
Gene Higgins <ehigginsiii@cnm.edu>
2017-04-18 14:56:43 -0600

    set min-width on .front-page-2 widgets

--- wp-content/themes/digital-pro/js/custom.js
+++ wp-content/themes/digital-pro/js/custom.js
@@ -3,11 +3,15 @@
  */
 jQuery(document).ready(function($) {
 	$(document).scroll(function(){
+
+		// hide fixed vertical menu when scroll is near bottom of page
 		if ($(document).scrollTop()/$(document).height() >=.85 ){
 			$('.sidebar.sidebar-primary.widget-area').css("position", "inherit");
 		} else {
 			$('.sidebar.sidebar-primary.widget-area').css("position", "fixed")
 		}
+
+
 
 	})
 })
