5295ffb20c62e81dd65826ce60ef28543da7ca3f
khofmann <khofmann1@cnn.edu>
2017-04-19 10:14:17 -0600

    added css

--- wp-content/themes/digital-pro/custom-functions.php
+++ wp-content/themes/digital-pro/custom-functions.php
@@ -13,4 +13,23 @@ function deep_dive_scripts() {
 	wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false);
 }
 add_action( 'wp_enqueue_scripts', 'deep_dive_scripts' );
+
+//google fonts 
+wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Roboto");
+
+}
+add_action( 'wp_enqueue_scripts', 'deep_dive_styles',100 );
+
+
+// cloud typography fonts
+    if (function_exists('is_wpe') && is_wpe()) {
+        wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/641408/css/fonts.css');
+    } else {
+        //development url
+        wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/785104/css/fonts.css');
+    }
+
+?>
+
+
 ?>
