6dda4763bf0d4f7e69e1c977b22d19f5a5f816a5
Gene Higgins <ehigginsiii@cnm.edu>
2017-04-19 16:13:28 -0600

    refactored the sidebar to be sticky and not run into either the header or the footer

--- wp-content/themes/digital-pro/js/custom.js
+++ wp-content/themes/digital-pro/js/custom.js
@@ -50,12 +50,12 @@ jQuery(document).ready(function($) {
 
 		var scrollPos = $(document).scrollTop()/$(document).height();
 
-		// hide fixed vertical menu when scroll is near bottom of page
-		if (scrollPos < .08 || scrollPos >=.82 || $(window).width() < 800  ){
-			$('.sidebar.sidebar-primary.widget-area').css("position", "inherit");
-		} else {
-				$('.sidebar.sidebar-primary.widget-area').css("position", "fixed")
-		}
+		// // hide fixed vertical menu when scroll is near bottom of page
+		// if (scrollPos < .08 || scrollPos >=.82 || $(window).width() < 800  ){
+		// 	$('.sidebar.sidebar-primary.widget-area').css("position", "inherit");
+		// } else {
+		// 		$('.sidebar.sidebar-primary.widget-area').css("position", "fixed")
+		// }
 
 		// show/hide sticky footer
 		if ( scrollPos >= .08 && scrollPos <= .85 ){
@@ -64,4 +64,46 @@ jQuery(document).ready(function($) {
 			$footer.fadeOut();
 		}
 	})
+
+	// Does the sidebar exist?
+	if(!!$('#genesis-sidebar-primary').offset()) {
+
+		// get elements
+		var $sidebar = $('#genesis-sidebar-primary');
+		var $sidebarInitialTop = $sidebar.offset().top;
+		var $header = $('header');
+
+		// create an element after the sidebar and before the footer
+		// that will detect a collision with the top of the page
+		$('#genesis-footer-widgets').before('<div class="sticky-stopper"></div>');
+		var $stickyStopper = $('.sticky-stopper');
+		// get the sticky-stopper's position, taking into account the height of the sidebar
+		var stopPoint = $stickyStopper.offset().top - $sidebar.innerHeight() ;
+
+		// start paying attention to the scroll event
+		$(document).scroll(function() {
+			var padding = 10;
+
+			// determine if wpadminbar is visible, if so add its height to padding
+			if (!!$('#wpadminbar').offset()) {
+				padding += $('#wpadminbar').height();
+			}
+
+			// add the header's height to padding
+			padding += $header.outerHeight();
+
+			if( stopPoint < $(window).scrollTop() + padding ){
+				// check if the stopPoint has hit the top of the page
+				// this is the bottom of the scroll
+				$sidebar.css({ position: 'absolute', top: stopPoint })
+			} else if ($sidebarInitialTop > $(window).scrollTop() + padding) {
+				// check if the sidebar's initial top has not yet scrolled to the top of the page
+				// this is the top of the scroll, i.e. the initial state
+				$sidebar.css({position: 'absolute', top: 'initial'})
+			} else {
+				// this is the scrolling behavior, i.e. fixed position
+				$sidebar.css({position: 'fixed', top:  padding})
+			}
+		})
+	}
 })
