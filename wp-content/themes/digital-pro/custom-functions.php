45fe56b6d1aed9e413fbf82a3aec3e852b933a26
khofmann <khofmann1@cnn.edu>
2017-04-19 10:26:14 -0600

    functions errors

--- wp-content/themes/digital-pro/custom-functions.php
+++ wp-content/themes/digital-pro/custom-functions.php
@@ -5,21 +5,9 @@ function deep_dive_styles() {
     wp_enqueue_style( 'custom-styles',
         get_stylesheet_directory_uri() . '/custom-styles.css'
     );
-}
-add_action( 'wp_enqueue_scripts', 'deep_dive_styles',100 );
-
-function deep_dive_scripts() {
-
-	wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false);
-}
-add_action( 'wp_enqueue_scripts', 'deep_dive_scripts' );
 
 //google fonts 
 wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Roboto");
-
-}
-add_action( 'wp_enqueue_scripts', 'deep_dive_styles',100 );
-
 
 // cloud typography fonts
     if (function_exists('is_wpe') && is_wpe()) {
@@ -29,7 +17,14 @@ add_action( 'wp_enqueue_scripts', 'deep_dive_styles',100 );
         wp_enqueue_style('typography-font', 'https://cloud.typography.com/6007112/785104/css/fonts.css');
     }
 
+}
+
+add_action( 'wp_enqueue_scripts', 'deep_dive_styles',100 );
+
+function deep_dive_scripts() {
+
+	wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false);
+}
+add_action( 'wp_enqueue_scripts', 'deep_dive_scripts' );
 ?>
 
-
-?>
